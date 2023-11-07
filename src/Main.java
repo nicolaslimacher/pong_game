097bc79a3292f89bb07f03062c5152aeb78a8fbf
nicolas.limacher <nicolas.limacher@sovos.com>
2023-11-07 14:43:06 -0500

    graphics runs, screen now black

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,14 @@ public class Main {
 
     static JFrame f = new JFrame("Pong");
     public static void main(String[] args) {
+        
         f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         f.setSize(650,495);
+
+        PongGame game = new PongGame();
+
+        f.add(game);
 
         //show that window
         f.setVisible(true);
--- src/PongGame.java
+++ src/PongGame.java
@@ -6,9 +6,9 @@ public class PongGame extends JPanel {
     static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
 
     public void paintComponent(Graphics g){
-        //background color
+   
         g.setColor(Color.black);
-        g.fillRect(x:0, y:0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        g.fillRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     }
 
 }
