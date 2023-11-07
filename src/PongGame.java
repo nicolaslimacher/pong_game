9753bf19f5a506899f04ea6b27a04882fc35ec1b
nicolas.limacher <nicolas.limacher@sovos.com>
2023-11-07 14:29:56 -0500

    Added graphics and background color

--- src/PongGame.java
+++ src/PongGame.java
@@ -1,3 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class PongGame extends JPanel {
     
+    static final int WINDOW_WIDTH = 640, WINDOW_HEIGHT = 480;
+
+    public void paintComponent(Graphics g){
+        //background color
+        g.setColor(Color.black);
+        g.fillRect(x:0, y:0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+
 }
