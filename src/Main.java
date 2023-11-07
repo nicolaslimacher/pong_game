b756463f33728a8f9b831f060fbbd5af3768938b
nicolas.limacher <nicolas.limacher@sovos.com>
2023-11-07 14:21:38 -0500

    Added window, added PongGame logic controller

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
 //basic hello world
-Class Main{
+public class Main {
 
-    public static void main(String[] args){
+    static JFrame f = new JFrame("Pong");
+    public static void main(String[] args) {
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-        System.out.println(x:"Hello, world.");
+        f.setSize(650,495);
+
+        //show that window
+        f.setVisible(true);
     }
 }
---
+++ src/PongGame.java
@@ -0,0 +1,3 @@
+public class PongGame extends JPanel {
+    
+}
